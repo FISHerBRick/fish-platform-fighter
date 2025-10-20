32512651485b7ff07b78566a352d7e2237ed458f
Shinobu <kocho4671@gmail.com>
2025-10-20 13:30:42 +1000

    Update game.js

--- game.js
+++ game.js
@@ -89,12 +89,12 @@ function update() {
 
 // --- Player Movement ---
 const step = 1; // move 1px at a time
-if(keys["d"]) {
+if(keys["a"]) {
   player.x += -20; // slower speed
   player.facingRight = true;
   moving = true;
 }
-if(keys["a"]) {
+if(keys["d"]) {
   player.x -= -20;
   player.facingRight = false;
   moving = true;
